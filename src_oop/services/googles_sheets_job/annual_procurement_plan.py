6ecadd42319bf36bf06832dbb6223bafc95e20d0
vuvuzela92 <kostyaostrom@gmail.com>
2026-03-18 11:49:06 +0300

    Добавил очистку данных от знаков валюты

--- src_oop/services/googles_sheets_job/annual_procurement_plan.py
+++ src_oop/services/googles_sheets_job/annual_procurement_plan.py
@@ -3,6 +3,7 @@ from src_oop.storage.google_sheets.google_sheets import delivery_calculation_chi
 import pandas as pd
 from datetime import datetime
 import gspread
+import numpy as np
 
 
 def transport_data_to_annual_procurement_plan():
@@ -17,18 +18,67 @@ def transport_data_to_annual_procurement_plan():
     df = pd.DataFrame(table_sheet_from_data[4:], # Данные начиная с 5-й строки
                     columns=table_sheet_from_data[3]) # Названия колонок с 4-й строки
 
+
+
+    def clean_currency_value(val):
+        """
+        Очищает строку от мусора и конвертирует в число.
+        """
+        # 1. Обрабатываем None, NaN и пустые значения
+        if val is None or (isinstance(val, float) and np.isnan(val)):
+            return np.nan
+        
+        # 2. Преобразуем в строку (списки и другие типы тоже обработаются)
+        val = str(val).strip()
+        
+        # 3. Пустая строка
+        if val == '' or val.lower() == 'nan':
+            return np.nan
+        
+        # 4. Удаляем знаки валют и все виды пробелов
+        for char in ['$', '€', '¥', '₽', 'RMB', 'руб', 'р.', ' ', ',', '\u00A0', '\t', '\n']:
+            val = val.replace(char, '')
+        
+        # 5. Заменяем запятую на точку
+        val = val.replace(',', '.')
+        
+        # 6. Конвертируем в число
+        try:
+            return float(val)
+        except (ValueError, TypeError):
+            return np.nan
+
+
+    # Список колонок с валютой
+    currency_columns = [
+        'Цена, RMB', 'Сумма заказа, RMB', 'Курс доллара', 'Курс юаня', 'Курс евро',
+        'Ст-ть АВТО или ЖД, $', 'сертификация, руб', 'доставка, RMB', 'пошлина, RMB', 'НДС, RMB', 
+        'сборы, RMB', 'страховка, RMB',	'брокеры, RMB',	'сертификаты',
+        'Расходы на доставку',	'Расходы на доставку 1 шт',
+        'Сумма аванса, RMB', 'Сумма аванса, РУБ'
+        'СУММА ИНВОЙСА ПОСТАВЩИКА, RMB', 'Последняя цена рынок', 'Себестоимость 1 шт. в руб ПЛАН', 'ИТОГО сумма поставки в себестоимости, руб. ПЛАН', 'Сумма баланса, RMB',
+        'Сумма баланса, РУБ'
+    ]
+
+    # Применяем очистку к нужным колонкам
+    for col in currency_columns:
+        if col in df.columns:
+            df[col] = df[col].apply(clean_currency_value)
+
+
+
+
     # Выбираем нужные колонки
     # choosen_columns = ["wild", "Статус", "Кол-во к заказу"]
-    cancel_statuses = ["отмена", "в планах", "прибыло"]
+    cancel_statuses = ["отмена", "в планах"]
     # Фильтрация
     df_short = df.loc[~df['Статус'].isin(cancel_statuses)]
     df_short["updatet_at"] = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
 
+    
     # Определяем таблицу и лист для вставки данных
     google_table_to = annual_procurement_plan.get("title")
     table_sheet_to = annual_procurement_plan.get("orders_sheet")
-
-
     # Создаем соединение с гугл-таблицей из таблицы из которой забираем данные
     try:
         # Создаем соединение с гугл-таблицей
