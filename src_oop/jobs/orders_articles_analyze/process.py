39398f9e9a246958b2499ff77201e981687346b0
vuvuzela92 <kostyaostrom@gmail.com>
2026-03-27 12:08:12 +0300

    Добавил обработку пропусков для дат и информации о товарах

--- src_oop/jobs/orders_articles_analyze/process.py
+++ src_oop/jobs/orders_articles_analyze/process.py
@@ -27,9 +27,25 @@ class ProcessArticleAnalyze:
 
         df = pd.merge(df_gen, df_adv, on=["article_id", "date"], how="left")
 
-        del df_adv
-        del df_gen
         
+        # === Обрабатываем пропуски в данных по товарам ===
+        # Создам набор данных по товарам из df_gen, чтобы заполнить пропуски в df
+        df_goods = df_gen[['account', 'local_vendor_code', 'article_id', 'subject_name']].drop_duplicates(subset=['article_id'])
+
+        # Удаляем вспомогательные датафреймы, чтобы освободить память
+        del df_adv
+        del df_gen        
+
+        # Заполняем каждую колонку отдельно через map
+        cols_to_fill = ['account', 'local_vendor_code', 'subject_name']
+
+        for col in cols_to_fill:
+            # Создаем "словарь" {article_id: значение_колонки}
+            mapping_dict = df_goods.set_index('article_id')[col]
+            
+            # Заполняем пропуски в основном df
+            df[col] = df[col].fillna(df['article_id'].map(mapping_dict))
+
         # Безопасное заполнение числовых колонок нулями
         numeric_cols = df.select_dtypes(include="number").columns
         df.loc[:, numeric_cols] = df[numeric_cols].fillna(0)
